b27f4b17ea8b7a2ba352048d047918493ca8d0d6
Dana <Dana.ackerman0914@gmail.com>
2024-07-06 23:22:28 +0500

    Added Test class and tested with Junit

---
+++ src/test/java/andersen/lesson/CustomQueueTest.java
@@ -0,0 +1,86 @@
+package andersen.lesson;
+
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+
+public class CustomQueueTest {
+
+    @Test
+    public void poll() {
+        Queue queue = new Queue();
+        queue.offer(1);
+        queue.offer(2);
+
+        Assertions.assertEquals(1, queue.poll());
+        Assertions.assertEquals(2, queue.poll());
+    }
+
+    @Test
+    public void remove() {
+        Queue queue = new Queue();
+        queue.offer(1);
+        queue.offer(2);
+
+        Assertions.assertTrue(queue.remove(1));
+        Assertions.assertTrue(queue.remove(2));
+    }
+    @Test
+    public void peek() {
+        Queue queue = new Queue();
+        queue.offer(1);
+        queue.offer(2);
+
+        Assertions.assertEquals(1, queue.peek());
+        Assertions.assertNotEquals(2, queue.peek());
+    }
+    @Test
+    public void contains() {
+        Queue q = new Queue();
+        q.add(1);
+        q.add(2);
+
+        Assertions.assertTrue(q.contains(1));
+        Assertions.assertTrue(q.contains(2));
+        Assertions.assertFalse(q.contains(3));
+    }
+    @Test
+    public void add() {
+        Queue queue = new Queue();
+
+        Assertions.assertTrue(queue.add(1));
+        Assertions.assertTrue(queue.add(2));
+    }
+    @Test
+    public void offer() {
+        Queue queue = new Queue();
+        Assertions.assertTrue(queue.offer(1));
+        Assertions.assertTrue(queue.offer(2));
+
+    }
+    @Test
+    public void remove2() {
+        Queue queue = new Queue();
+        queue.offer(1);
+        queue.offer(2);
+
+        Assertions.assertTrue(queue.remove(1));
+        Assertions.assertTrue(queue.remove(2));
+
+    }
+    @Test
+    public void element() {
+        Queue queue = new Queue();
+        queue.offer(1);
+        queue.offer(2);
+        queue.offer(3);
+
+        Assertions.assertEquals(1, queue.element());
+        Assertions.assertNotEquals(2, queue.element());
+        Assertions.assertNotEquals(3, queue.element());
+
+    }
+
+}
+
